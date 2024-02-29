389ceadf6565fa272afa258fd473abda4f8f400a
Shir Brass <shirbr510@gmail.com>
2024-02-29 21:10:41 +0200

    move ttl to constant

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,11 +3,13 @@ import Counter from "./Counter";
 import TEXTS from "../Texts";
 import Age from "./Age";
 
+const ITEM_TTL = 60;
+
 function Item({ id, onDeleteItem }) {
   const deleteItem = useCallback(() => onDeleteItem(id),[onDeleteItem, id])
   return (
     <li>
-      <Age maxAge={60} onMaxAge={deleteItem} />
+      <Age maxAge={ITEM_TTL} onMaxAge={deleteItem} />
       <Counter/>
       <button onClick={deleteItem}>{TEXTS.DELETE}</button>
     </li>
